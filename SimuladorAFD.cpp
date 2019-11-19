ec599ff4f2f8b8704944d20c251633f3dee11d2b
Mauro <m.j.98@outlook.com>
2019-11-19 19:13:56 -0300

    Mais testes com o mesmo automato podem ser feitos.

--- SimuladorAFD.cpp
+++ SimuladorAFD.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-void imprimeTbl();
+void ImprimeAFD();
 void imprimeCL(string entrada, int estadoAtual, int posicao);
 void Resultado(bool aceito, string casoTeste);
 
@@ -19,29 +19,32 @@ struct Regra{
 //***
 int main() {
 	
-	imprimeTbl();
+ ImprimeAFD();
 
 	return 0;
 }
 //***
 
-void imprimeTbl(){
+void ImprimeAFD(){
 
 	int estadoFinal = 1;
 	int tamEstados = 2;
 	
 	
-	// string alfabeto("01");
-	string alfabeto("01");
+	// string 
+	string alfabeto;
 
   string casoTeste("00010110");
 
   Regra Estado[tamEstados];
   
-	// CASOS DE TESTES de exemplo
-  //Cadeia com numero par de 0s
- 
-	Estado[1].transicao[0].simbolo = '0'; //Ex. Estado(1) com simbolo de transição 0 
+  /*
+	CASOS DE TESTES DE EXEMPLO
+
+  Cadeia com numero par de 0s
+  alfabeto("01");
+  
+	Estado[1].transicao[0].simbolo = '0';     //Ex. Estado(1) com simbolo de transição 0 
 	Estado[1].transicao[0].proximoEstado = 2; // continua no estado(1)
 	Estado[1].transicao[1].simbolo = '1';
 	Estado[1].transicao[1].proximoEstado = 1;
@@ -50,6 +53,7 @@ void imprimeTbl(){
 	Estado[2].transicao[0].proximoEstado = 1;
 	Estado[2].transicao[1].simbolo = '1';
 	Estado[2].transicao[1].proximoEstado = 2;
+  */
 
 
 	cout << "----SIMULADOR DE AUTOMATO FINITO----\n" << endl;
@@ -88,73 +92,95 @@ void imprimeTbl(){
 	for(int i = 1;i <= tamEstados;i++){
 		for(int j = 0; j < alfabeto.length(); j++){
 			cout << "Estado "  << i << " com transição em \"" << alfabeto[j] << "\" muda para o Estado:" <<endl;
-			// Estado[i].Alfabeto[j] = alfabeto[j];
 			Estado[i].transicao[j].simbolo = alfabeto[j];
 			cin >> Estado[i].transicao[j].proximoEstado;
 		}
 	}
 	cout << "\n=============================" << endl;
 
-	
-	cout << "\n5. Informe o caso de teste: " << endl;
-	cin >> casoTeste;
+  bool aceito = false;
 
-	cout << "\n--- Tamanho da entrada: " << casoTeste.length() << " ---\n" << endl;
+  char testes = 's';
+	while(testes == 's'){
+
+    cout << "\n--- Alfabeto: {";
+	  for(int i = 0; i < alfabeto.length(); i++){
+		  cout << alfabeto[i];
+		  if(i != alfabeto.length() - 1){
+			  cout << " , ";
+		  }
+	  }
+	  cout << "} ---" <<endl;
+
+    cout << "\n--- Estado Final: " << estadoFinal << " ---" << endl;
+    
+
+    cout << "\n5. Informe o caso de teste: (sem espaços) " << endl;
+    cin >> casoTeste;
+
+    cout << "\n--- Tamanho da entrada: " << casoTeste.length() << " ---\n" << endl;
 
 
-	char elemento;
-	
-	bool aceito = false;
-	int encontrado = 0;
+    char elemento;
+    
+    
+    int encontrado = 0;
 
-	for(int i = 0; i < casoTeste.length(); i++){
-		elemento = casoTeste[i];
-		
-		encontrado = alfabeto.find(elemento);
-	
-		if(encontrado  >= 0){
-			//cout << i << ": " << elemento << " ENCONTRADO" << endl;
-			aceito = true;
-		}else{
-			cout << "O Elemento: \"" << elemento << "\" Não pertence ao alfabeto informado!" << endl;
-			aceito = false;
-			break;
-		}
-	}
+    for(int i = 0; i < casoTeste.length(); i++){
+      elemento = casoTeste[i];
+      
+      encontrado = alfabeto.find(elemento);
+    
+      if(encontrado  >= 0){
+        aceito = true;
+      }else{
+        cout << "O Elemento: \"" << elemento << "\" Não pertence ao alfabeto informado!" << endl;
+        aceito = false;
+        break;
+      }
+    }
 
-	int posicao = 0, estadoAtual = 1;
+    int posicao = 0, estadoAtual = 1;
+    
 
-	while(aceito){
+    
+    if(aceito == true){
+      cout << "\n=== MUDANÇAS DE ESTADO ===" << endl;
+      for(int i = 0; i <= casoTeste.length(); i++,posicao++){
 
-		cout << "\n=== MUDANÇAS DE ESTADO ===" << endl;
-		for(int i = 0; i <= casoTeste.length(); i++,posicao++){
+        imprimeCL(casoTeste, estadoAtual, posicao);
 
-			imprimeCL(casoTeste, estadoAtual, posicao);
+        elemento = casoTeste[i];
 
-			elemento = casoTeste[i];
-
-			for(int j = 0; j < alfabeto.length(); j++){
-				if(Estado[estadoAtual].transicao[j].simbolo == elemento){
-					estadoAtual = Estado[estadoAtual].transicao[j].proximoEstado;
-					//cout << "EstadoAtual: " << estadoAtual << endl;
-				}
-				if(posicao == casoTeste.length()){ //Fim do Teste
-          if(estadoFinal == estadoAtual){
-            cout << "=== Fim do teste ===" << endl;
-            aceito = true;
-            break;
-          }else{
-            cout << "=== Fim do teste ===" << endl;
-            aceito = false;
-            break;
+        for(int j = 0; j < alfabeto.length(); j++){
+          if(Estado[estadoAtual].transicao[j].simbolo == elemento){
+            estadoAtual = Estado[estadoAtual].transicao[j].proximoEstado;
+          }
+          if(posicao == casoTeste.length()){ //Fim do Teste
+            if(estadoFinal == estadoAtual){
+              cout << "=== Fim do teste ===" << endl;
+              aceito = true;
+              break;
+            }else{
+              cout << "=== Fim do teste ===" << endl;
+              aceito = false;
+              break;
+            }
           }
         }
-			}
-		}
-		break;
-	}
-		
-	Resultado(aceito,casoTeste);
+      }
+    }
+    Resultado(aceito,casoTeste);
+    
+    cout << "\n\nDeseja realizar outro teste?(S , N)";
+    cin >> testes;
+    testes = tolower(testes);
+
+    cout << "*limpando a tela*" << endl;
+    system("clear||cls");
+  }
+  // ---------------------------------------------
+	
  } // Fim Função AFD
 
 void imprimeCL(string entrada, int estadoAtual, int posicao) {
